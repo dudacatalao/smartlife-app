0542ef845471c2e0839f9ab2473314d69f78ef15
Maria Eduarda de Melo Catal� <51550887807@>
2024-04-08 11:28:51 -0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import Navbar2 from './components/Navbar2'
 
 function App() {
   return (
-    <Navbar/>
+    <Navbar2/>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/styles/*.css" 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        emerald: colors.emerald,
+        green1: '#87a878',
+        green2: '#b0bc98',
+        background:'#2C2C2C'
+      }
+    },
   },
   plugins: [],
 }
